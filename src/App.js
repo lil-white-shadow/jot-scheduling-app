b9a1b6421b70370852c42889ff729e356aae8166
lil-white-shadow <kru.patel.swe@gmail.com>
2023-01-09 01:48:06 -0600

    add state in App, add form in CreateEvent

--- src/App.js
+++ src/App.js
@@ -8,13 +8,22 @@ export default function App() {
   const title = "Doodle-but-Better";
 
   const [newEventId, setNewEventId] = useState('');
+  const [eventName, setEventName] = useState('Volleyball');
+  const [eventDate, setEventDate] = useState(new Date().toLocaleDateString());
+  const [eventStartTime, setEventStartTime] = useState('07:30');
+  const [eventEndTime, setEventEndTime] = useState('09:00');
+  const [eventLocation, setEventLocation] = useState('The Club at Prairie Stone, Hoffman Estates');
 
   return (
     <div>
     <BrowserRouter>
       <Routes>
           <Route path="/" element={<Home title={title} setNewEventId={setNewEventId}/>} />
-          <Route path={"eventId=" + newEventId} element={<CreateEvent title={title} newEventId={newEventId}/>} />
+          <Route
+          /*  path={"eventId=" + newEventId} */
+          // temp route for dev
+           path="event"
+           element={<CreateEvent newEventId={newEventId} eventName={eventName} setEventName={setEventName} eventDate={eventDate} setEventDate={setEventDate} eventStartTime={eventStartTime} setEventStartTime={setEventStartTime} eventEndTime={eventEndTime} setEventEndTime={setEventEndTime} eventLocation={eventLocation} setEventLocation={setEventLocation}/>} />
       </Routes>
     </BrowserRouter>
     <div>Parent: {newEventId}</div>
--- src/components/CreateEvent.js
+++ src/components/CreateEvent.js
@@ -1,7 +1,46 @@
 export default function CreateEvent(props) {
+
+  let formInputs = {
+    "eventName": props.eventName,
+    "eventDate": props.eventDate,
+    "eventStartTime": props.eventStartTime,
+    "eventEndTime": props.eventEndTime,
+    "eventLocation": props.eventLocation
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+    console.log(formInputs)
+  }
   return (
     <div>
-      Create your event! Your event id is {props.newEventId}
+      <h1>
+        Create your event!
+      </h1>
+      <h2>Your event id is {props.newEventId}</h2>
+      <form onSubmit={onSubmit} noValidate>
+        <div>
+        <label htmlFor="eventName">Name: </label>
+        <input type="text" name="eventName" defaultValue={props.eventName} onChange={e => props.setEventName(e.target.value)}/>
+        </div>
+        <div>
+          <label htmlFor="eventDate">Date: </label>
+          <input type="date" name="eventDate" defaultValue={props.eventDate} onChange={e => props.setEventDate(e.target.value)}/>
+        </div>
+        <div>
+          <label htmlFor="eventStartTime">Start: </label>
+          <input type="time" name="eventStartTime" defaultValue={props.eventStartTime} onChange={e => props.setEventStartTime(e.target.value)}/>
+        </div>
+        <div>
+          <label htmlFor="eventEndTime">End: </label>
+          <input type="time" name="eventEndTime" defaultValue={props.eventEndTime} onChange={e => props.setEventEndTime(e.target.value)}/>
+        </div>
+        <div>
+          <label htmlFor="eventLocation">Location: </label>
+          <input type="address" name="eventLocation" defaultValue={props.eventLocation} onChange={e => props.setEventLocation(e.target.value)}/>
+        </div>
+        <button type="submit">Create</button>
+      </form>
     </div>
   )
 }
