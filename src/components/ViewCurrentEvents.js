be4da7e5d2bbcb64b3d58e4d8b69cca78e748ec1
lil-white-shadow <kru.patel.swe@gmail.com>
2023-01-10 14:52:13 -0600

    style ViewCurrentEvents

--- src/components/ViewCurrentEvents.js
+++ src/components/ViewCurrentEvents.js
@@ -2,20 +2,26 @@ import { Link } from "react-router-dom";
 
 export default function ViewCurrrentEvents(props) {
   return (
-  <div className="main main__ViewCurrentEvents">
-      <h2>There are currently {props.currentEvents.length} events in progress.</h2>
-      {
-        props.currentEvents.map(event =>
-          <div key={event.eventId}>
-            <span>
-              <Link to={"/view-eventId=" + event.eventId}>{event.eventId}</Link>
-            </span>
-            <span>
-            : {event.eventName} on {event.eventDate} by {event.eventOrganizer}
-            </span>
-          </div>
-        )
-      }
+  <div className="main main__ViewCurrentEvents card">
+      <h1>{props.currentEvents.length} Events in progress</h1>
+      <div className="cardLineGroup">
+        {
+          props.currentEvents.map(event =>
+            <div key={event.eventId} className="cardLine">
+              <span className="cardLineTitle">
+              {event.eventId} - {event.eventName} on {event.eventDate} by {event.eventOrganizer}
+              </span>
+              <button className="goToButton">
+              <Link to={"/view-eventId=" + event.eventId}>
+                Go to event
+                &nbsp;
+                <div className="goToArrow">&#8627;</div>
+              </Link>
+              </button>
+            </div>
+          )
+        }
+      </div>
   </div>
   )
 }
