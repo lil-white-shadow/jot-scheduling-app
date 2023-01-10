b5116431e06daa037810b242e73709cd942e257f
lil-white-shadow <kru.patel.swe@gmail.com>
2023-01-10 14:02:24 -0600

    style - ViewEvent

--- src/components/ViewEvent.js
+++ src/components/ViewEvent.js
@@ -33,32 +33,49 @@ export default function ViewEvent(props) {
   return(
     <div className="main main__ViewEvent">
       <h1>You've been invited!</h1>
-      <div>
+      <div className="viewEventCard">
         <h2>Event: {props.currentEvent.eventName}</h2>
-        <p>Date: {props.currentEvent.eventDate}</p>
-        <p>Time: {props.currentEvent.eventStartTime} - {props.currentEvent.eventEndTime}</p>
-        <p>Location: {props.currentEvent.eventLocation}</p>
-        <p>Organized by: {props.currentEvent.eventOrganizer}</p>
+        <div className="cardLine">
+          <div className="cardLineTitle">Date: </div>
+          <div className="cardLineContent">{props.currentEvent.eventDate}</div>
+        </div>
+        <div className="cardLine">
+          <div className="cardLineTitle">Time: </div>
+          <div className="cardLineContent">{props.currentEvent.eventStartTime} - {props.currentEvent.eventEndTime} {props.currentEvent.eventEndTime < "12:00" ? "am" : null}</div>
+        </div>
+        <div className="cardLine">
+          <div className="cardLineTitle">Location: </div>
+          <div className="cardLineContent">{props.currentEvent.eventLocation}</div>
+        </div>
+        <div className="cardLine">
+          <div className="cardLineTitle">Host: </div>
+          <div className="cardLineContent">{props.currentEvent.eventOrganizer}</div>
+        </div>
+        {/* <h2>Event: {props.currentEvent.eventName}</h2>
+        <p><span className="viewEvent__label">Date: </span>{props.currentEvent.eventDate}</p>
+        <p><span className="viewEvent__label">Time: </span> {props.currentEvent.eventStartTime} - {props.currentEvent.eventEndTime}</p>
+        <p><span className="viewEvent__label">Location: </span> {props.currentEvent.eventLocation}</p>
+        <p><span className="viewEvent__label">Host: </span> {props.currentEvent.eventOrganizer}</p> */}
       </div>
       {
         !isFormSubmitted ? 
         <div>
-        <h2>Are you attending?</h2>
-        <form onSubmit={onSubmit} noValidate>
-          <div>
-            <label htmlFor="userName">Name: </label>
+        <form className="viewEventCard" onSubmit={onSubmit} noValidate>
+          <h2>Are you attending?</h2>
+          <div className="cardLine">
+            <label htmlFor="userName" className="cardLineTitle">Name: </label>
             <input type="text" name="userName" onChange={e => setUserName(e.target.value)}/>
           </div>
-          <div>
-            <label htmlFor="availability">Availability: </label>
+          <div className="cardLine">
+            <label htmlFor="availability" className="cardLineTitle">Availability: </label>
             <select name="availability" onChange={e => setAvailability(e.target.value)} defaultValue="">
               <option disabled value="">Select</option>
               <option value="Yes">Yes</option>
               <option value="No">No</option>
             </select>
           </div>
-          <div>
-            <label htmlFor="guestStatus">Guest?: </label>
+          <div className="cardLine">
+            <label htmlFor="guestStatus" className="cardLineTitle">Guest?: </label>
             <input type="checkbox" name="guestStatus" onChange={e => setGuestStatus(e.target.value)}/>
           </div>
           <button type="submit">Submit</button>
@@ -66,27 +83,34 @@ export default function ViewEvent(props) {
       </div>
       : null
       }
-
-      <h2>{props.availableUsers.length} members & {props.specialInvitees.length} guest(s)</h2>
-      <div>
-        <h4>Members: </h4>
-        <ul>
-        {
-          props.availableUsers.map(user => <li key={user}>{user}</li>)
-        }
-        </ul>
-        <h4>Guests:</h4>
-        <ul>
-          {
-            props.specialInvitees.map(user => <li key={user}>{user}</li>)
-          }
-        </ul>
-        <h4>Unavailable: </h4>
-        <ul>
-          {
-            props.unavailableUsers.map(user => <li key={user}>{user}</li>)
-          }
-        </ul>
+      <div className="viewEventCard">
+        <h2>{props.availableUsers.length} members & {props.specialInvitees.length} guest(s)</h2>
+        <div className="cardLineGroup">
+          <div className="cardLine">
+            <div className="cardLineTitle">Members: </div>
+            <ul>
+            {
+              props.availableUsers.map(user => <li key={user}>{user}</li>)
+            }
+            </ul>
+          </div>
+          <div className="cardLine">
+            <div className="cardLineTitle">Guests: </div>
+            <ul>
+              {
+                props.specialInvitees.map(user => <li key={user}>{user}</li>)
+              }
+            </ul>
+          </div>
+          <div className="cardLine">
+            <div className="cardLineTitle">Unavailable: </div>
+            <ul>
+              {
+                props.unavailableUsers.map(user => <li key={user}>{user}</li>)
+              }
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   )
