f0067329064c1693bbb9e0083e8adb5dd2675c8f
lil-white-shadow <kru.patel.swe@gmail.com>
2023-01-10 20:39:10 -0600

    update About wording slightly

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
       <h1>Our Promise</h1>
       <p><b>JOT</b> is just the easiest way to schedule your next event for free. It is minimalistic but has all the features you will ever need.</p>
       <p>
-        If you insist on creating an account, giving your name, email, phone or credit card information, then <b>JOT</b> is <span style={{textDecoration: "underline"}}>not</span> for you.
+        If you insist on creating an account and giving your email, phone or credit card information, then <b>JOT</b> is <span style={{textDecoration: "underline"}}>not</span> for you.
       </p>
       <p>
         Yes, you read that right, what are you waiting for? Let's get your next party started ...
