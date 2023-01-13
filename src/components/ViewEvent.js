a8261c4a9334d3720d2134be1524d58ae8be122d
lil-white-shadow <kru.patel.swe@gmail.com>
2023-01-13 17:28:32 -0600

    fix ViewEvent bug resetting RSVP info on reload
    (cherry picked from commit 7fc8212f179cc93ceb5917ee5c8bbff20487631b)

--- src/components/ViewEvent.js
+++ src/components/ViewEvent.js
@@ -3,14 +3,12 @@ import { useState, useEffect } from "react";
 export default function ViewEvent(props) {
 
   const [currentEvent, setCurrentEvent] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isSendingRSVP, setIsSendingRSVP] = useState(false);
 
   const [userName, setUserName] = useState('');
   const [availability, setAvailability] = useState(undefined);
   const [guestStatus, setGuestStatus] = useState('off');
-
-  const [availableUsers, setAvailableUsers] = useState([]);
-  const [specialInvitees, setSpecialInvitees] = useState([]);
-  const [unavailableUsers, setUnavailableUsers] = useState([]);
 
   const [isFormValid, setIsFormValid] = useState(false);
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
@@ -27,51 +25,50 @@ export default function ViewEvent(props) {
 
     if(formInputs.userName !== '' && formInputs.availability !== undefined) {
       setIsFormValid(true);
+      setIsSendingRSVP(true);
 
-      let availableArray = availableUsers.filter(name => name !== formInputs.userName.toUpperCase());
-      let specialInviteesArray = specialInvitees.filter(name => name !== formInputs.userName.toUpperCase());
-      let unavailableArray = unavailableUsers.filter(name => name !== formInputs.userName.toUpperCase());
+      let availableArray = currentEvent.eventAttendees.filter(name => name !== formInputs.userName.toUpperCase());
+      let specialInviteesArray = currentEvent.eventSpecialGuests.filter(name => name !== formInputs.userName.toUpperCase());
+      let unavailableArray = currentEvent.eventNonAttendees.filter(name => name !== formInputs.userName.toUpperCase());
 
-      if(formInputs.availability === 'Yes' && formInputs.guestStatus === 'off' && formInputs.userName.toUpperCase) {
+      if(formInputs.availability === 'Yes' && formInputs.guestStatus === 'off') {
 
-        setAvailableUsers([...availableArray, formInputs.userName.toUpperCase()]);
-        if(specialInvitees !== specialInviteesArray) {
-          setSpecialInvitees([...specialInviteesArray]);
-        }
-        if(unavailableUsers !== unavailableArray) {
-          setUnavailableUsers([...unavailableArray]);
-        }
+        currentEvent.eventAttendees = [...availableArray, formInputs.userName.toUpperCase()];
+        currentEvent.eventSpecialGuests = [...specialInviteesArray];
+        currentEvent.eventNonAttendees = [...unavailableArray];
 
       } else if(formInputs.availability === 'Yes' && formInputs.guestStatus === 'on') {
         
-        setSpecialInvitees([...specialInviteesArray,formInputs.userName.toUpperCase()]);
-
-        if(availableUsers !== availableArray) {
-          setAvailableUsers([...availableArray]);
-        }
-        if(unavailableUsers !== unavailableArray) {
-          setUnavailableUsers([...unavailableArray]);
-        }
+        currentEvent.eventAttendees = [...availableArray];
+        currentEvent.eventSpecialGuests = [...specialInviteesArray, formInputs.userName.toUpperCase()];
+        currentEvent.eventNonAttendees = [...unavailableArray];
 
       } else {
 
-        setUnavailableUsers([...unavailableArray, formInputs.userName.toUpperCase()]);
-
-        if(availableUsers !== availableArray) {
-          setAvailableUsers([...availableArray]);
-        }
-        if(specialInvitees !== specialInviteesArray) {
-          setSpecialInvitees([...specialInviteesArray]);
-        }      
+        currentEvent.eventAttendees = [...availableArray];
+        currentEvent.eventSpecialGuests = [...specialInviteesArray];
+        currentEvent.eventNonAttendees = [...unavailableArray, formInputs.userName.toUpperCase()];  
       }
-  
+
+      fetch(props.url + '/api/events/' + props.eventId, {
+        method: 'PATCH',
+        headers:{'content-type': 'application/json'},
+        body: JSON.stringify({
+          eventAttendees: currentEvent.eventAttendees,
+          eventSpecialGuests: currentEvent.eventSpecialGuests,
+          eventNonAttendees: currentEvent.eventNonAttendees
+        })
+      }).then(() => {
+        setTimeout(() => setIsSendingRSVP(false), 2000)
+      })
+
       setUserName('');
       setAvailability(undefined);
       setGuestStatus('off');
       
-      } else {
-        setIsFormValid(false);
-      }
+    } else {
+      setIsFormValid(false);
+    }
   }
 
   // get event details
@@ -80,119 +77,151 @@ export default function ViewEvent(props) {
       .then(response => response.json())
       .then(data => {
         setCurrentEvent(data[0]);
-
-        // DO NOT USE currentEvent.eventAttendees etc. as they still picks up empty [] from original state
-        setAvailableUsers(data[0].eventAttendees);
-        setSpecialInvitees(data[0].eventSpecialGuests);
-        setUnavailableUsers(data[0].eventNonAttendees);
+        setTimeout(() => setIsLoading(false), 2000)
       })
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  // update event RSVP
-  useEffect(() => {
-
-    fetch(props.url + '/api/events/' + props.eventId, {
-      method: 'PATCH',
-      headers:{'content-type': 'application/json'},
-    body: JSON.stringify({
-      eventAttendees: availableUsers,
-      eventSpecialGuests: specialInvitees,
-      eventNonAttendees: unavailableUsers
-    })
-  })
-// eslint-disable-next-line react-hooks/exhaustive-deps
-}, [availableUsers, specialInvitees, unavailableUsers])
-
   
-  return(
+  return (
     <div className="main main__ViewEvent">
-      <h1>You've been invited!</h1>
-      <div className="card">
-        <h2>Event: {currentEvent.eventName}</h2>
-        <div className="cardLine">
-          <div className="cardLineTitle">Date: </div>
-          <div className="cardLineContent">{currentEvent.eventDate}</div>
-        </div>
-        <div className="cardLine">
-          <div className="cardLineTitle">Time: </div>
-          <div className="cardLineContent">{currentEvent.eventStartTime} - {currentEvent.eventEndTime} {currentEvent.eventEndTime < "12:00" ? "am" : null}</div>
-        </div>
-        <div className="cardLine">
-          <div className="cardLineTitle">Location: </div>
-          <div className="cardLineContent">{currentEvent.eventLocation}</div>
-        </div>
-        <div className="cardLine">
-          <div className="cardLineTitle">Host: </div>
-          <div className="cardLineContent">{currentEvent.eventOrganizer}</div>
-        </div>
-      </div>
-      {
-        !isFormSubmitted || !isFormValid ? 
-        <div>
-        <form className="card" onSubmit={onSubmit} noValidate>
-          <h2>Are you attending?</h2>
-          <div className="cardLine">
-            <label htmlFor="userName" className="cardLineTitle">Name: </label>
-            <input type="text" name="userName" onChange={e => setUserName(e.target.value)}/>
+      {isLoading ? (
+        <div id="loading"></div>
+      ) : (
+        <>
+          <h1>You've been invited!</h1>
+          <div className="card">
+            <h2>Event: {currentEvent.eventName}</h2>
+            <div className="cardLine">
+              <div className="cardLineTitle">Date: </div>
+              <div className="cardLineContent">{currentEvent.eventDate}</div>
+            </div>
+            <div className="cardLine">
+              <div className="cardLineTitle">Time: </div>
+              <div className="cardLineContent">
+                {currentEvent.eventStartTime} - {currentEvent.eventEndTime}{" "}
+                {currentEvent.eventEndTime < "12:00" ? "am" : null}
+              </div>
+            </div>
+            <div className="cardLine">
+              <div className="cardLineTitle">Location: </div>
+              <div className="cardLineContent">
+                {currentEvent.eventLocation}
+              </div>
+            </div>
+            <div className="cardLine">
+              <div className="cardLineTitle">Host: </div>
+              <div className="cardLineContent">
+                {currentEvent.eventOrganizer}
+              </div>
+            </div>
           </div>
-          <div className="cardLine">
-            <label htmlFor="availability" className="cardLineTitle">Availability: </label>
-            <select name="availability" onChange={e => setAvailability(e.target.value)} defaultValue="">
-              <option disabled value="">Select</option>
-              <option value="Yes">Yes</option>
-              <option value="No">No</option>
-            </select>
-          </div>
-          <div className="cardLine">
-            <label htmlFor="guestStatus" className="cardLineTitle">Are you a non-member (guest)?: </label>
-            <input type="checkbox" name="guestStatus" onChange={e => setGuestStatus(e.target.value)}/>
-          </div>
-          <div className="cardLine">
-            <button type="submit">Submit</button></div>
-            {
-              !isFormValid && isFormSubmitted ?
-              <span style={{fontWeight: "700", textDecoration: "underline"}}>Error! You must enter your name & availability.</span>
-              : null
-            }
-        </form>
-      </div>
-      : <div className="feedback">
-        <h2>You're all set &#10004;</h2>
-        <div>
-          Click <span onClick={() => setIsFormSubmitted(false)} style={{textDecoration: "underline", cursor:"pointer"}}>here</span> to make changes or RSVP for someone else.
-        </div>
-      </div>
-      }
-      <div className="card">
-        <h2>{availableUsers.length} members & {specialInvitees.length} guests</h2>
-        <div className="cardLineGroup">
-          <div className="cardLine cardLineVertical">
-            <div className="cardLineTitle">Members: </div>
-            <ul>
-            {
-              availableUsers.map(user => <li key={user}>{user}</li>)
-            }
-            </ul>
-          </div>
-          <div className="cardLine cardLineVertical">
-            <div className="cardLineTitle">Guests: </div>
-            <ul>
-              {
-                specialInvitees.map(user => <li key={user}>{user}</li>)
-              }
-            </ul>
-          </div>
-          <div className="cardLine cardLineVertical">
-            <div className="cardLineTitle">Unavailable: </div>
-            <ul>
-              {
-                unavailableUsers.map(user => <li key={user}>{user}</li>)
-              }
-            </ul>
-          </div>
-        </div>
-      </div>
+          {!isFormSubmitted || !isFormValid ? (
+            <div>
+              <form className="card" onSubmit={onSubmit} noValidate>
+                <h2>Are you attending?</h2>
+                <div className="cardLine">
+                  <label htmlFor="userName" className="cardLineTitle">
+                    Name:{" "}
+                  </label>
+                  <input
+                    type="text"
+                    name="userName"
+                    onChange={(e) => setUserName(e.target.value)}
+                  />
+                </div>
+                <div className="cardLine">
+                  <label htmlFor="availability" className="cardLineTitle">
+                    Availability:{" "}
+                  </label>
+                  <select
+                    name="availability"
+                    onChange={(e) => setAvailability(e.target.value)}
+                    defaultValue=""
+                  >
+                    <option disabled value="">
+                      Select
+                    </option>
+                    <option value="Yes">Yes</option>
+                    <option value="No">No</option>
+                  </select>
+                </div>
+                <div className="cardLine">
+                  <label htmlFor="guestStatus" className="cardLineTitle">
+                    Are you a non-member (guest)?:{" "}
+                  </label>
+                  <input
+                    type="checkbox"
+                    name="guestStatus"
+                    onChange={(e) => setGuestStatus(e.target.value)}
+                  />
+                </div>
+                <div className="cardLine">
+                  <button type="submit">Submit</button>
+                </div>
+                {!isFormValid && isFormSubmitted ? (
+                  <span
+                    style={{ fontWeight: "700", textDecoration: "underline" }}
+                  >
+                    Error! You must enter your name & availability.
+                  </span>
+                ) : null}
+              </form>
+            </div>
+          ) : (
+            <div className="feedback">
+              <h2>You're all set &#10004;</h2>
+              <div>
+                Click{" "}
+                <span
+                  onClick={() => setIsFormSubmitted(false)}
+                  style={{ textDecoration: "underline", cursor: "pointer" }}
+                >
+                  here
+                </span>{" "}
+                to make changes or RSVP for someone else.
+              </div>
+            </div>
+          )}
+          {
+          isSendingRSVP ? 
+            <div id="loading"></div>
+          :
+            <div className="card">
+              <h2>
+                {currentEvent.eventAttendees.length} members &{" "}
+                {currentEvent.eventSpecialGuests.length} guests
+              </h2>
+              <div className="cardLineGroup">
+                <div className="cardLine cardLineVertical">
+                  <div className="cardLineTitle">Members: </div>
+                  <ul>
+                    {currentEvent.eventAttendees.map((user) => (
+                      <li key={user}>{user} </li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="cardLine cardLineVertical">
+                  <div className="cardLineTitle">Guests: </div>
+                  <ul>
+                    {currentEvent.eventSpecialGuests.map((user) => (
+                      <li key={user}>{user}</li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="cardLine cardLineVertical">
+                  <div className="cardLineTitle">Unavailable: </div>
+                  <ul>
+                    {currentEvent.eventNonAttendees.map((user) => (
+                      <li key={user}>{user}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          }
+        </>
+      )}
     </div>
-  )
+  );
 }
