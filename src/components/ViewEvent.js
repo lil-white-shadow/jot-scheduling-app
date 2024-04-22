05db1278a9821f3158d2b9bcaf5091c6ad7d48f0
lil-white-shadow <kru.patel.swe@gmail.com>
2024-04-22 11:09:32 -0500

    add day of the week, update formatting

--- src/components/ViewEvent.js
+++ src/components/ViewEvent.js
@@ -1,82 +1,93 @@
 import { useState, useEffect } from "react";
 
 export default function ViewEvent(props) {
-
   const [currentEvent, setCurrentEvent] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isSendingRSVP, setIsSendingRSVP] = useState(false);
 
-  const [userName, setUserName] = useState('');
+  const [userName, setUserName] = useState("");
   const [availability, setAvailability] = useState(undefined);
-  const [guestStatus, setGuestStatus] = useState('off');
+  const [guestStatus, setGuestStatus] = useState("off");
 
   const [isFormValid, setIsFormValid] = useState(false);
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
 
   let formInputs = {
-    "userName": userName,
-    "availability": availability,
-    "guestStatus": guestStatus
-  }
-  
+    userName: userName,
+    availability: availability,
+    guestStatus: guestStatus,
+  };
+
   function onSubmit(e) {
     e.preventDefault();
     setIsFormSubmitted(true);
 
-    if(formInputs.userName !== '' && formInputs.availability !== undefined) {
+    if (formInputs.userName !== "" && formInputs.availability !== undefined) {
       setIsFormValid(true);
       setIsSendingRSVP(true);
 
       // request event data from server - again - to check for any updates since initial page load
       fetch(props.url + "/api/events/" + props.eventId)
-      .then(response => response.json())
-      .then(data => {
-        setCurrentEvent(data[0]);
-        // remove duplicate name if present in any category
-        let availableArray = data[0].eventAttendees.filter(name => name !== formInputs.userName.toUpperCase());
-        let specialInviteesArray = data[0].eventSpecialGuests.filter(name => name !== formInputs.userName.toUpperCase());
-        let unavailableArray = data[0].eventNonAttendees.filter(name => name !== formInputs.userName.toUpperCase());
-  
+        .then((response) => response.json())
+        .then((data) => {
+          setCurrentEvent(data[0]);
+          // remove duplicate name if present in any category
+          let availableArray = data[0].eventAttendees.filter(
+            (name) => name !== formInputs.userName.toUpperCase()
+          );
+          let specialInviteesArray = data[0].eventSpecialGuests.filter(
+            (name) => name !== formInputs.userName.toUpperCase()
+          );
+          let unavailableArray = data[0].eventNonAttendees.filter(
+            (name) => name !== formInputs.userName.toUpperCase()
+          );
 
-        // add name to the applicable category
-        if(formInputs.availability === 'Yes') {
-          if(formInputs.guestStatus === 'off') {
-            availableArray = [...availableArray, formInputs.userName.toUpperCase().trim()];
+          // add name to the applicable category
+          if (formInputs.availability === "Yes") {
+            if (formInputs.guestStatus === "off") {
+              availableArray = [
+                ...availableArray,
+                formInputs.userName.toUpperCase().trim(),
+              ];
+            } else {
+              specialInviteesArray = [
+                ...specialInviteesArray,
+                formInputs.userName.toUpperCase().trim(),
+              ];
+            }
           } else {
-            specialInviteesArray = [...specialInviteesArray, formInputs.userName.toUpperCase().trim()];
+            unavailableArray = [
+              ...unavailableArray,
+              formInputs.userName.toUpperCase().trim(),
+            ];
           }
-        } else {
-          unavailableArray  = [...unavailableArray, formInputs.userName.toUpperCase().trim()];  
-        }
-  
-        // send updated RSVP to server
-        fetch(props.url + '/api/events/' + props.eventId, {
-          method: 'PATCH',
-          headers:{'content-type': 'application/json'},
-          body: JSON.stringify({
-            eventAttendees: availableArray,
-            eventSpecialGuests: specialInviteesArray,
-            eventNonAttendees: unavailableArray
-          })
-        })
-        .then(() => {
-          // request event data from server to update view
-          fetch(props.url + "/api/events/" + props.eventId)
-          .then(response => response.json())
-          .then(data => {
-            setCurrentEvent(data[0]);
-          })
-        })
-        .then(() => {
-          setTimeout(() => setIsSendingRSVP(false), 2000)
-        })
-  
-        setUserName('');
-        setAvailability(undefined);
-        setGuestStatus('off');
 
-      })
-      
+          // send updated RSVP to server
+          fetch(props.url + "/api/events/" + props.eventId, {
+            method: "PATCH",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify({
+              eventAttendees: availableArray,
+              eventSpecialGuests: specialInviteesArray,
+              eventNonAttendees: unavailableArray,
+            }),
+          })
+            .then(() => {
+              // request event data from server to update view
+              fetch(props.url + "/api/events/" + props.eventId)
+                .then((response) => response.json())
+                .then((data) => {
+                  setCurrentEvent(data[0]);
+                });
+            })
+            .then(() => {
+              setTimeout(() => setIsSendingRSVP(false), 2000);
+            });
+
+          setUserName("");
+          setAvailability(undefined);
+          setGuestStatus("off");
+        });
     } else {
       setIsFormValid(false);
     }
@@ -85,17 +96,26 @@ export default function ViewEvent(props) {
   // request event data on initial page load
   useEffect(() => {
     fetch(props.url + "/api/events/" + props.eventId)
-      .then(response => response.json())
-      .then(data => {
-        if(data[0] !== currentEvent) {
+      .then((response) => response.json())
+      .then((data) => {
+        if (data[0] !== currentEvent) {
           setCurrentEvent(data[0]);
-          setTimeout(() => setIsLoading(false), 2000)
+          setTimeout(() => setIsLoading(false), 2000);
         }
-      })
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  
+  function getDay(dateString) {
+    const date = new Date(dateString);
+    date.setDate(date.getDate() + 1);
+    const dayOfWeek = date.toLocaleString("en-us", { weekday: "long" });
+
+    return dayOfWeek;
+  }
+
+  console.log(currentEvent);
+
   return (
     <div className="main main__ViewEvent">
       {isLoading ? (
@@ -104,10 +124,17 @@ export default function ViewEvent(props) {
         <>
           <h1>You've been invited!</h1>
           <div className="card">
-            <h2>{currentEvent.eventName} ({currentEvent.eventAttendees.length + currentEvent.eventSpecialGuests.length} attendees)</h2>
+            <h2>
+              {currentEvent.eventName} (
+              {currentEvent.eventAttendees.length +
+                currentEvent.eventSpecialGuests.length}{" "}
+              attendees)
+            </h2>
             <div className="cardLine">
               <div className="cardLineTitle">Date: </div>
-              <div className="cardLineContent">{currentEvent.eventDate}</div>
+              <div className="cardLineContent">
+                {currentEvent.eventDate}, {getDay(currentEvent.eventDate)}
+              </div>
             </div>
             <div className="cardLine">
               <div className="cardLineTitle">Time: </div>
@@ -181,10 +208,8 @@ export default function ViewEvent(props) {
                 ) : null}
               </form>
             </div>
-          ) : null
-          }
-          {
-            isFormSubmitted && isFormValid && !isSendingRSVP ? 
+          ) : null}
+          {isFormSubmitted && isFormValid && !isSendingRSVP ? (
             <div className="feedback">
               <h2>You're all set &#10004;</h2>
               <div>
@@ -197,17 +222,18 @@ export default function ViewEvent(props) {
                 </span>{" "}
                 to make changes or RSVP for someone else.
               </div>
-            </div> : null
-          }
-          {
-          isSendingRSVP ? 
+            </div>
+          ) : null}
+          {isSendingRSVP ? (
             <div id="loading"></div>
-          :
+          ) : (
             <div className="card">
               <h2>RSVP Details</h2>
               <div className="cardLineGroup cardLineGroupVertical">
                 <div className="cardLine cardLineVertical">
-                  <div className="cardLineTitle">Members - {currentEvent.eventAttendees.length}</div>
+                  <div className="cardLineTitle">
+                    Members - {currentEvent.eventAttendees.length}
+                  </div>
                   <ul>
                     {currentEvent.eventAttendees.map((user) => (
                       <li key={user}>{user} </li>
@@ -215,7 +241,9 @@ export default function ViewEvent(props) {
                   </ul>
                 </div>
                 <div className="cardLine cardLineVertical">
-                  <div className="cardLineTitle">Guests - {currentEvent.eventSpecialGuests.length}</div>
+                  <div className="cardLineTitle">
+                    Guests - {currentEvent.eventSpecialGuests.length}
+                  </div>
                   <ul>
                     {currentEvent.eventSpecialGuests.map((user) => (
                       <li key={user}>{user}</li>
@@ -223,7 +251,9 @@ export default function ViewEvent(props) {
                   </ul>
                 </div>
                 <div className="cardLine cardLineVertical">
-                  <div className="cardLineTitle">Unavailable - {currentEvent.eventNonAttendees.length}</div>
+                  <div className="cardLineTitle">
+                    Unavailable - {currentEvent.eventNonAttendees.length}
+                  </div>
                   <ul>
                     {currentEvent.eventNonAttendees.map((user) => (
                       <li key={user}>{user}</li>
@@ -232,7 +262,7 @@ export default function ViewEvent(props) {
                 </div>
               </div>
             </div>
-          }
+          )}
         </>
       )}
     </div>
