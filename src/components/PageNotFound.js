f246b989e0cf58596366a9bc7c26c5d4d1eade21
lil-white-shadow <kru.patel.swe@gmail.com>
2024-04-22 11:31:52 -0500

    add timeout before showing page not found message

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 export default function PageNotFound() {
-  return(
+  const [isLoading, setIsLoading] = useState(true);
+
+  setTimeout(() => setIsLoading(false), 5000);
+  return isLoading ? (
+    <div id="loading"></div>
+  ) : (
     <div className="main">
-      <h1>S#^@, you got me!</h1>
-      <p>It seems like you have the wrong address. This page does not exist on our servers.</p>
+      <h1>Page not found</h1>
+      <p>
+        It seems like you have the wrong address. This page does not exist on
+        our servers.
+      </p>
     </div>
-  )
+  );
 }
